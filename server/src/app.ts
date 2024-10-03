3c915e4195c54f30f4dd7f25db212104388b612f
sonson-arch <sondrhso@stud.ntnu.no>
2024-10-03 22:01:47 +0200

    #

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,16 +8,5 @@ const app = express();
 
 app.use(express.json());
 
-// Ny rute for root URL ("/") - kan brukes til enkel testing eller velkomstmelding
-app.get('/', (req, res) => {
-  res.send('Velkommen til serveren! Prøv å poste til /api/v2/run.');
-});
-
-// Siden API-et ikke er kompatibelt med v1, øk API-versjon til v2
+// Since API is not compatible with v1, API version is increased to v2
 app.use('/api/v2', router);
-
-// Start serveren
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
-export default app;
